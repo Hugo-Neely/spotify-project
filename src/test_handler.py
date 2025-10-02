b4f36e0cda78ed54fb647b69ce06d6116c3e0ab1
esemsc-htn223 <hugo.neely24@imperial.ac.uk>
2025-10-02 09:04:03 +0100

    add date column to test data file

--- src/test_handler.py
+++ src/test_handler.py
@@ -13,9 +13,9 @@ def create_test_file(mpl_handler):
     test_file = 'playlists_2200-01-01.csv'
     with open(os.path.join(mpl_handler.data_dir, test_file), 'w') as f:
         f.write(
-'''id,name,description,n_tracks,href,images,snapshot_id
-aaaaaa,Nov-2199,testtesttest,500,https://example.com,"[{'height': None, 'url': 'https://example.com', 'width': None}]",bbbbbbbb
-aaaaaa,Dec-2199,testtesttest,500,https://example.com,"[{'height': None, 'url': 'https://example.com', 'width': None}]",bbbbbbbb''')
+'''id,date,name,description,n_tracks,href,images,snapshot_id
+aaaaaa,2199-11-01,Nov-2199,testtesttest,500,https://example.com,"[{'height': None, 'url': 'https://example.com', 'width': None}]",bbbbbbbb
+aaaaaa,2199-12-01,Dec-2199,testtesttest,500,https://example.com,"[{'height': None, 'url': 'https://example.com', 'width': None}]",bbbbbbbb''')
 
     return test_file
 
