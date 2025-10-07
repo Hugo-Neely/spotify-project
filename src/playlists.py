fe6220787d5eb5c52d894bf048e6570548b49a24
esemsc-htn223 <hugo.neely24@imperial.ac.uk>
2025-10-07 11:42:33 +0100

    add logging to keep track of API rate limits, as well as misc actions of the spotify client

--- src/playlists.py
+++ src/playlists.py
@@ -2,12 +2,60 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
+import time
 import datetime
 from typing import Union, Tuple
 import numpy as np
 import requests
 from io import BytesIO
 import matplotlib.pyplot as plt
+import logging
+
+# ensure data directory is set up as expected
+DATA_DIR = os.path.abspath('data')
+IMG_DIR = os.path.join(DATA_DIR, 'imgs')
+LOG_DIR = os.path.join(DATA_DIR, 'logs')
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+if not os.path.exists(IMG_DIR):
+    os.makedirs(IMG_DIR)
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger_csv_formatter = logging.Formatter('%(asctime)s.%(msecs)d,%(message)s', datefmt='%Y-%m-%d %H:%M:%S', style ='%')
+logger_console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# tracking API calls to CSV
+logger_calls_fh = logging.FileHandler(os.path.join(LOG_DIR, 'spotify_api_calls.csv'))
+if not os.path.exists(os.path.join(LOG_DIR, 'spotify_api_calls.csv')):
+    with open(os.path.join(LOG_DIR, 'spotify_api_calls.csv'), 'w') as f:
+        f.write('time,message\n')
+logger_calls_fh.setLevel(logging.DEBUG)
+logger_calls_fh.set_name('spotify_api_calls')
+logger_calls_fh.setFormatter(logger_csv_formatter)
+logger_calls_fh.addFilter(lambda record: record.levelname == 'DEBUG')
+logger.addHandler(logger_calls_fh)
+
+# tracking other actions to file
+logger_fh = logging.FileHandler(os.path.join(LOG_DIR, 'playlist_handler_info.log'))
+if not os.path.exists(os.path.join(LOG_DIR, 'playlist_handler_info.log')):
+    with open(os.path.join(LOG_DIR, 'playlist_handler_info.log'), 'w') as f:
+        f.write('time,message\n')
+logger_fh.setLevel(logging.INFO)
+logger_fh.set_name('playlist_handler_info')
+logger_fh.setFormatter(logger_console_formatter)  # use same formatter as console
+logger.addHandler(logger_fh)
+
+# logging warnings to console
+logger_ch = logging.StreamHandler()
+logger_ch.setLevel(logging.WARNING)
+logger_ch.set_name('console')
+logger_ch.setFormatter(logger_console_formatter)
+logger.addHandler(logger_ch)
+
 
 class MonthlyPlaylistHandler:
     '''
@@ -25,7 +73,7 @@ class MonthlyPlaylistHandler:
                    '2024':'-24',
                    '2025':'2025'}
     
-    data_dir = os.path.abspath('data')
+    data_dir = DATA_DIR
     
     def __init__(self, spotify_client:spotipy.Spotify = None):
         '''
@@ -38,8 +86,13 @@ class MonthlyPlaylistHandler:
             a SpotifyOAuth manager as the auth_manager
         '''
         if spotify_client is None:
-            spotify_client = self._new_spotify_client()
-        self.spotify_client = spotify_client
+            self.spotify_client = self._new_spotify_client()
+        elif isinstance(spotify_client, LoggingSpotifyClient):
+            self.spotify_client = spotify_client
+        elif isinstance(spotify_client, spotipy.Spotify):
+            self.spotify_client = LoggingSpotifyClient(spotify_client = spotify_client)
+        else:
+            raise ValueError(f'Expected spotify_client to be of type spotipy.Spotify, got {type(spotify_client)}.')
 
     @staticmethod
     def _new_spotify_client(client_id:str = None, client_secret:str = None, redirect_uri:str = None,
@@ -73,7 +126,7 @@ class MonthlyPlaylistHandler:
         if redirect_uri is None:
             redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
         
-        return spotipy.Spotify(
+        return LoggingSpotifyClient(
             auth_manager=spotipy.SpotifyOAuth(
                 scope="playlist-read-private", 
                 client_id=client_id,
@@ -105,7 +158,7 @@ class MonthlyPlaylistHandler:
                                    'name', 'owner', 'primary_color', 'public', 'snapshot_id', 'tracks',
                                    'type', 'uri'])
         while not got_all_pls:
-            playlists = self.spotify_client.current_user_playlists(offset = offset)
+            playlists = self.spotify_client.current_user_playlists(limit = 50, offset = offset)
             df = pd.concat([df, pd.DataFrame(playlists['items'])], ignore_index = True)
             
             if len(playlists['items']) < 50:
@@ -147,7 +200,7 @@ class MonthlyPlaylistHandler:
         df['date'] = pd.to_datetime(df['date']).dt.date
 
         # extract image url
-        df['cover_image_url'] = df['images'].apply(lambda x: x[0]['url'] if not pd.isna(x) else x)
+        df['cover_image_url'] = df['images'].apply(lambda x: x[0]['url'] if not pd.isna(x[0]) else x)
 
         # rename href to url
         df = df.rename(columns={'href':'url'})
@@ -261,7 +314,7 @@ class MonthlyPlaylistHandler:
             raise ValueError(f'No monthly playlist found for ID {playlist_id}.')
         return pl_date
 
-    def get_playlist_cover_image(self, playlist_id:str = None, playlist_date:Union[str, datetime.date] = None,
+    def get_playlist_cover_image(self, *args, playlist_id:str = None, playlist_date:Union[str, datetime.date] = None,
                                  overwrite:bool = False, errors:str = 'raise') -> np.ndarray:
         '''
         Get the cover image for a monthly playlist, either by its Spotify ID or date.
@@ -287,6 +340,20 @@ class MonthlyPlaylistHandler:
         np.ndarray
             The cover image as a numpy array, or None if no cover image exists.
         '''
+        if args:
+            if len(args) == 1:
+                if isinstance(args[0], str):
+                    if len(args[0]) == 10 and args[0][4] == '-' and args[0][7] == '-':
+                        playlist_date = args[0]
+                    else:
+                        playlist_id = args[0]
+                elif isinstance(args[0], datetime.date):
+                    playlist_date = args[0]
+                else:
+                    raise ValueError(f'Unexpected type "{type(args[0])}" for date/ID input encountered. Try entering date as either a string (YYYY-MM-DD) or datetime.date, or ID as a string.')
+            else:
+                raise ValueError('Only one positional argument is accepted, either playlist_id or playlist_date.')
+
 
         # check inputs are valid
         if (playlist_id is None) and (playlist_date is None):
@@ -349,15 +416,25 @@ class MonthlyPlaylistHandler:
         plt.imsave(save_path, im)
         return im
     
-    def plot_playlist_covers(self, scale:float = 1.0) -> Tuple[plt.Figure, np.ndarray]:
+    def plot_playlist_covers(self, orientation:str = 'horizontal', 
+                             scale:float = 1.0, wspace:float = 0.1, hspace:float = 0.1) -> Tuple[plt.Figure, np.ndarray]:
         '''
         Plot a collage of all monthly playlist cover images.
 
         Parameters
         ----------
+        orientation : {'horizontal', 'vertical'}
+            If 'horizontal' (default), the plot will have years as rows and months as columns.
+            If 'vertical', the plot will have months as rows and years as columns.
         scale : float
             A scaling factor to apply to the size of each image in the plot. Default is 1.0.
             Applied to the figsize parameter of plt.subplots().
+        wspace : float
+            The amount of width reserved for space between subplots, expressed as a fraction
+            of the average axis width. Default is 0.1.
+        hspace : float
+            The amount of height reserved for space between subplots, expressed as a fraction
+            of the average axis height. Default is 0.1.
         
         Returns
         -------
@@ -371,10 +448,18 @@ class MonthlyPlaylistHandler:
         min_year = df_pl['date'].apply(lambda x: x.year).min()
         n_months = df_pl['date'].apply(lambda x: x.month).nunique()  # should be 12 but this covers the special case of a new user with less than a years data
 
-        fig, axes = plt.subplots(n_months, n_years, figsize=(n_years*scale, n_months*scale))
+        if orientation == 'horizontal':
+            n_rows, n_cols = n_years, n_months
+        else:
+            n_rows, n_cols = n_months, n_years
+        fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*scale, n_rows*scale))
 
-        for m, months_ax in enumerate(axes):
-            for y, ax in enumerate(months_ax):
+        for row_i, months_axs in enumerate(axes):
+            for col_i, ax in enumerate(months_axs):
+                if orientation == 'horizontal':
+                    y, m = row_i, col_i
+                else:
+                    y, m = col_i, row_i
                 date = f'{min_year+y}-{m+1:02d}-01'
                 im = self.get_playlist_cover_image(playlist_date=date, errors='ignore')
                 if im is not None:
@@ -382,7 +467,7 @@ class MonthlyPlaylistHandler:
 
                 ax.axis('off')
         
-        fig.subplots_adjust(wspace=0, hspace=0)
+        fig.subplots_adjust(wspace=wspace, hspace=hspace)
 
         return fig, axes
     
@@ -402,7 +487,138 @@ class MonthlyPlaylistHandler:
             pad2 = diff - pad1
             return im[:, pad1:-pad2, :]
 
+    def get_tracks(self):
+        '''
+        Placeholder for getting tracks in each monthly playlist.
+        '''
+        pass
 
 
+class LoggingSpotifyClient(spotipy.Spotify):
+    '''
+    Wrapper class to keep track of Spotify API calls.
+    '''
 
+    RATE_LIMIT_PER_30S = 90
+    log_filepath = os.path.join(LOG_DIR, 'spotify_api_calls.csv')
+
+    def __init__(self, spotify_client = None, *args, **kwargs):
+        '''
+        Create a new `LoggingSpotifyClient` instance, optionally wrapping an existing
+        `spotipy.Spotify` instance. See `spotipy.Spotify` documentation for
+        accepted args and kwargs.
+
+        Parameters
+        ----------
+        spotify_client : spotipy.Spotify, optional
+            The spotify client to wrap the logger around. If None, a new instance
+            will be created using the provided args and kwargs.
+        '''
+        self.log_action('Initialized new LoggingSpotifyClient instance.')
+
+        if spotify_client is not None:
+            if not isinstance(spotify_client, spotipy.Spotify):
+                raise ValueError(f'Expected spotify_client to be of type spotipy.Spotify, got {type(spotify_client)}.')
+            self.__dict__.update(spotify_client.__dict__)
+            self.__class__ = type('LoggingSpotifyClient', (self.__class__, spotify_client.__class__), {})
+        else:
+            super().__init__(*args, **kwargs)
         
+        self.clean_log()  # clear old log entries
+
+    def _internal_call(self, method, url, payload, params):
+        self.log_call(method, check_rate=True)
+        return super()._internal_call(method, url, payload, params)
+    
+    def log_call(self, call_name, check_rate:bool = True, **check_rate_kwargs):
+        '''
+        Log the call time and type to the log file. 
+
+        Parameters
+        ----------
+        call_name : str
+            The name of the API call being made.
+        check_rate : bool
+            If True (default), will check the current API rate and sleep if necessary
+            to avoid exceeding the rate limit.
+        **check_rate_kwargs
+            Optional keyword arguments to pass to `check_api_rate()`.
+        '''
+        logger.debug(call_name)
+        if check_rate:
+            self.check_api_rate(**check_rate_kwargs)
+
+    @property
+    def df_log(self) -> pd.DataFrame:
+        '''
+        Read the API call log file into a DataFrame.
+
+        Returns
+        -------
+        pandas.DataFrame
+            The log file as a DataFrame.
+        '''
+        return pd.read_csv(self.log_filepath, parse_dates=['time'])
+    
+    @property
+    def api_rate(self) -> int:
+        '''
+        Check how many API calls have been made in the last 30 second window.
+
+        Returns
+        -------
+        int
+            The number of API calls made in the last 30 seconds.
+        '''
+        window_s = 30
+        df_log = self.df_log
+        now = pd.Timestamp.now()
+        window_start = now - pd.Timedelta(seconds=window_s)
+        return df_log[df_log['time'] >= window_start].shape[0]
+
+    def check_api_rate(self, limit_proportion:float = 0.8,
+                       sleep_time:float = 5.0, warn:bool = True):
+        '''
+        Determine if the current API rate is below `limit_proportion` of the approximate allowed threshold (180 calls/min). If
+        over the threshold, sleep for `sleep_time` seconds and check again, repeating until the
+        rate is below the threshold.
+
+        Parameters
+        ----------
+        limit_proportion : float
+            The proportion of the rate limit to check against (default is 0.8 for 80%).
+        sleep_time : float
+            The number of seconds to sleep for if the API rate is over the limit.
+        '''
+        near_limit = self.api_rate > (self.RATE_LIMIT_PER_30S * limit_proportion)  # 80% of limit
+
+        if near_limit:
+            if warn:
+                logger.warning(f'API rate limit exceeded ({self.api_rate} > {self.RATE_LIMIT_PER_30S}), sleeping for {sleep_time} seconds.')
+            time.sleep(sleep_time)
+            self.check_api_rate(sleep_time=sleep_time, # keep checking until we're under the limit
+                                limit_proportion=limit_proportion, 
+                                warn=False)  # don't keep warning I don't want to go crazy
+            
+    def clean_log(self):
+        '''
+        Delete all previous log entries that are not within the last 30 seconds.
+        '''
+        window_s = 30
+        df_log = self.df_log
+        now = pd.Timestamp.now()
+        window_start = now - pd.Timedelta(seconds=window_s)
+        df_log = df_log[df_log['time'] >= window_start]
+        df_log.to_csv(self.log_filepath, index=False)
+        self.log_action('Cleaned old log entries.')
+    
+    def log_action(self, message:str):
+        '''
+        Log a non-API action to the log file.
+
+        Parameters
+        ----------
+        message : str
+            The message to log.
+        '''
+        logger.info(message)
